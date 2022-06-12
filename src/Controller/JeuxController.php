c8ab669c466b65abf0c92808ec16f06b360d3075
Emilie Gregory <emiliegregory@hotmail.com>
2022-06-12 10:28:22 +0200

    Add show page and navigation between pages

---
+++ src/Controller/JeuxController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Jeu;
+use App\Repository\JeuRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class JeuxController extends AbstractController
+{
+    //Route pour accéder à la page d'accueil
+    #[Route('/', name: 'app_home')]
+    public function index(JeuRepository $jeuRepository): Response
+    {
+        $jeux = $jeuRepository->findAll();
+        return $this->render('jeux/index.html.twig', compact('jeux'));
+    }
+
+    //Route pour accéder à la page détail de chaque jeu, l'ID est mis en paramêtre de la route, il contiendra des chiffres obligatoirement
+    #[Route('/jeux/{id<[0-9]+>}', name: 'app_jeux_show')]
+    public function show(Jeu $jeu): Response
+    {
+        return $this->render('jeux/show.html.twig', compact('jeu'));
+    }
+}
