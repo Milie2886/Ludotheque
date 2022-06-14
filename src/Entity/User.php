4a64ac23f75d8c44f9d6e6018f87dbcdbf08b470
Emilie Gregory <emiliegregory@hotmail.com>
2022-06-14 15:40:52 +0200

    ajout methode pour concatener le prenom et le nom (fullName) utilise dans index et show

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -169,5 +169,10 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
+    public function getFullName(): ?string
+    {
+        return $this->getFirstName() . ' ' . $this->getLastName();
+    }
+
 
 }
