c2dc495af26def51a83cb37526245de2fe78ce42
Emilie Gregory <emiliegregory@hotmail.com>
2022-06-18 07:02:22 +0200

    redirection vers accueil si déjà connecté

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -30,6 +30,12 @@ class RegistrationController extends AbstractController
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, LoginFormAuthenticator $authenticator, EntityManagerInterface $em): Response
     {
+        if ($this->getUser()) {
+            $this->addFlash('error', 'Vous êtes déjà connecté(e)!');
+
+            return $this->redirectToRoute('app_home');
+        }
+        
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
